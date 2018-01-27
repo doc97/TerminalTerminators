2fda014aa0f3eb6b3134118a1c29512d339f8739
specnats <specnats@hotmail.fi>
2018-01-27 23:48:29 +0000

    SOUND WORKS

--- MainMenu.js
+++ MainMenu.js
@@ -7,11 +7,7 @@ BasicGame.MainMenu.prototype = {
 
     create: function () {
 
-        // We've already preloaded our assets, so let's kick right into the Main
-		// Menu itself.
-        // Here all we're doing is playing some music and adding a picture and
-		// button
-        // Naturally I expect you to do something significantly better :)
+       
         this.sound.play('track2');
         playButton = this.add.button(this.camera.x + this.camera.width/2, this.camera.y + this.camera.height/2, 'playButton', function(){this.state.start('Game');this.sound.stopAll()}, this);
         playButton.anchor.setTo(0.5, 0.5);
