a5614d470596be685a08d4e9810027d9bcb24891
doc97 <daniel.riissanen@gmail.com>
2018-01-27 18:28:56 +0200

    Clean up

--- Game.js
+++ Game.js
@@ -126,57 +126,19 @@ class Terminal {
 
 
 /* Game code */
-var content = [
-    "The sky above the port was the color of television, tuned to a dead channel.",
-    "`It's not like I'm using,' Case heard someone say, as he shouldered his way ",
-    "through the crowd around the door of the Chat. `It's like my body's developed",
-    "this massive drug deficiency.' It was a Sprawl voice and a Sprawl joke.",
-    "The Chatsubo was a bar for professional expatriates; you could drink there for",
-    "a week and never hear two words in Japanese.",
-    "",
-    "Ratz was tending bar, his prosthetic arm jerking monotonously as he filled a tray",
-    "of glasses with draft Kirin. He saw Case and smiled, his teeth a webwork of",
-    "East European steel and brown decay. Case found a place at the bar, between the",
-    "unlikely tan on one of Lonny Zone's whores and the crisp naval uniform of a tall",
-    "African whose cheekbones were ridged with precise rows of tribal scars. `Wage was",
-    "in here early, with two joeboys,' Ratz said, shoving a draft across the bar with",
-    "his good hand. `Maybe some business with you, Case?'",
-    "",
-    "Case shrugged. The girl to his right giggled and nudged him.",
-    "The bartender's smile widened. His ugliness was the stuff of legend. In an age of",
-    "affordable beauty, there was something heraldic about his lack of it. The antique",
-    "arm whined as he reached for another mug.",
-    "",
-    "",
-    "From Neuromancer by William Gibson"
-];
-
-var line = [];
-
-var wordIndex = 0;
-var lineIndex = 0;
-
-var wordDelay = 120;
-var lineDelay = 400;
-
-
 BasicGame.Game = function (game) {
 
     // When a State is added to Phaser it automatically has the following
-	// properties set on it, even if they already exist:
+    // properties set on it, even if they already exist:
 
     this.game;      // a reference to the currently running game (Phaser.Game)
-    this.add;       // used to add sprites, text, groups, etc
-					// (Phaser.GameObjectFactory)
+    this.add;       // used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
     this.camera;    // a reference to the game camera (Phaser.Camera)
     this.cache;     // the game cache (Phaser.Cache)
-    this.input;     // the global input manager. You can access
-					// this.input.keyboard, this.input.mouse, as well from it.
-					// (Phaser.Input)
+    this.input;     // the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
     this.load;      // for preloading assets (Phaser.Loader)
     this.math;      // lots of useful common math operations (Phaser.Math)
-    this.sound;     // the sound manager - add a sound, play one, set-up
-					// markers, etc (Phaser.SoundManager)
+    this.sound;     // the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
     this.stage;     // the game stage (Phaser.Stage)
     this.time;      // the clock (Phaser.Time)
     this.tweens;    // the tween manager (Phaser.TweenManager)
@@ -185,15 +147,9 @@ BasicGame.Game = function (game) {
     this.particles; // the particle manager (Phaser.Particles)
     this.physics;   // the physics manager (Phaser.Physics)
     this.rnd;       // the repeatable random number generator
-					// (Phaser.RandomDataGenerator)
 
-    // You can use any of these from any function within this State.
-    // But do consider them as being 'reserved words', i.e. don't create a
-    // property for your own game called "world" or you'll over-write the world
-    // reference.
 
     this.view = 0; // 0 = terminal, 1 = map
-
     this.switch = function() {
         if (this.view == 0) {
             console.log(this.view);
@@ -212,8 +168,6 @@ BasicGame.Game.prototype = {
     
     create: function () {
         // Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-        // text = this.add.text(32, 32, '', { font: "15px Arial", fill: "#19de65" });
-        // this.nextLine();
 
         // Camera origin: upper-left
         this.camera.setSize(640, 480);
@@ -240,9 +194,7 @@ BasicGame.Game.prototype = {
 	        menu.anchor.setTo(0.5, 0.5);
                 menu.fixedToCamera = true;
 	
-	        // And a label to illustrate which menu item was chosen. (This is
-			// not
-			// necessary)
+	        // And a label to illustrate which menu item was chosen. (This is not necessary)
 	        choiseLabel = this.add.text(this.camera.width/2, 30, 'Click outside to continue', { font: '30px Arial', fill: '#ffffff' });
 	        choiseLabel.anchor.setTo(0.5, 0.5);
                 choiseLabel.fixedToCamera = true;
@@ -263,9 +215,7 @@ BasicGame.Game.prototype = {
 	
 	            // Check if the click was inside the menu
 	            if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
-	                // The choicemap is an array that will help us see which
-					// item
-					// was clicked
+	                // The choicemap is an array that will help us see which item was clicked
 	                var choisemap = ['one', 'two', 'three', 'four'];
 	
 	                // Get menu local coordinates for the click
@@ -300,8 +250,7 @@ BasicGame.Game.prototype = {
     },
 
     update: function () {
-        // Honestly, just about anything could go here. It's YOUR game after
-		// all. Eat your heart out!
+        // Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
     }, 
 
     render: function() {
