9fe45a8352f9a6e0bbc7ebdc4815376980b734ff
specnats <specnats@hotmail.fi>
2018-01-27 11:39:57 +0000

    WIP

--- Game.js
+++ Game.js
@@ -75,51 +75,123 @@ var lineIndex = 0;
 var wordDelay = 120;
 var lineDelay = 400;
 
+
 BasicGame.Game = function (game) {
 
-    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+    // When a State is added to Phaser it automatically has the following
+	// properties set on it, even if they already exist:
 
-    this.game;      //  a reference to the currently running game (Phaser.Game)
-    this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
-    this.camera;    //  a reference to the game camera (Phaser.Camera)
-    this.cache;     //  the game cache (Phaser.Cache)
-    this.input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
-    this.load;      //  for preloading assets (Phaser.Loader)
-    this.math;      //  lots of useful common math operations (Phaser.Math)
-    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
-    this.stage;     //  the game stage (Phaser.Stage)
-    this.time;      //  the clock (Phaser.Time)
-    this.tweens;    //  the tween manager (Phaser.TweenManager)
-    this.state;     //  the state manager (Phaser.StateManager)
-    this.world;     //  the game world (Phaser.World)
-    this.particles; //  the particle manager (Phaser.Particles)
-    this.physics;   //  the physics manager (Phaser.Physics)
-    this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+    this.game;      // a reference to the currently running game (Phaser.Game)
+    this.add;       // used to add sprites, text, groups, etc
+					// (Phaser.GameObjectFactory)
+    this.camera;    // a reference to the game camera (Phaser.Camera)
+    this.cache;     // the game cache (Phaser.Cache)
+    this.input;     // the global input manager. You can access
+					// this.input.keyboard, this.input.mouse, as well from it.
+					// (Phaser.Input)
+    this.load;      // for preloading assets (Phaser.Loader)
+    this.math;      // lots of useful common math operations (Phaser.Math)
+    this.sound;     // the sound manager - add a sound, play one, set-up
+					// markers, etc (Phaser.SoundManager)
+    this.stage;     // the game stage (Phaser.Stage)
+    this.time;      // the clock (Phaser.Time)
+    this.tweens;    // the tween manager (Phaser.TweenManager)
+    this.state;     // the state manager (Phaser.StateManager)
+    this.world;     // the game world (Phaser.World)
+    this.particles; // the particle manager (Phaser.Particles)
+    this.physics;   // the physics manager (Phaser.Physics)
+    this.rnd;       // the repeatable random number generator
+					// (Phaser.RandomDataGenerator)
 
-    //  You can use any of these from any function within this State.
-    //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+    // You can use any of these from any function within this State.
+    // But do consider them as being 'reserved words', i.e. don't create a
+	// property for your own game called "world" or you'll over-write the world
+	// reference.
 };
 
 BasicGame.Game.prototype = {
     
     create: function () {
-        // Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-        //text = this.add.text(32, 32, '', { font: "15px Arial", fill: "#19de65" });
+        // Honestly, just about anything could go here. It's YOUR game after
+		// all. Eat your heart out!
+        // text = this.add.text(32, 32, '', { font: "15px Arial", fill:
+		// "#19de65" });
         terminal = new Terminal(this);
-        //this.nextLine();
+        // this.nextLine();
+
+        /*
+		 * Code for the pause menu
+		 */
+
+        // Create a label to use as a button
+        pauseButton = this.add.button(this.world.width/2, this.world.height/2, 'pauseButton', function(str) {
+	    	// When the pause button is pressed, we pause the game
+	        this.paused = true;
+	
+	        // Then add the menu
+	        menu = this.add.sprite(this.world.width/2, this.world.height/2, 'menu');
+	        menu.anchor.setTo(0.5, 0.5);
+	
+	        // And a label to illustrate which menu item was chosen. (This is
+			// not
+			// necessary)
+	        choiseLabel = this.add.text(this.world.width/2, 30, 'Click outside menu to continue', { font: '30px Arial', fill: '#ffffff' });
+	        choiseLabel.anchor.setTo(0.5, 0.5);
+	    }, this);
+        pauseButton.anchor.setTo(0.5, 0.5);
+	
+	    // Add a input listener that can help us return from being paused
+	    this.input.onDown.add(unpause, this);
+	
+	    // And finally the method that handels the pause menu
+	    function unpause(event){
+	        // Only act if paused
+	        if(this.paused){
+	            // Calculate the corners of the menu
+	            var x1 = this.world.width/2 - menu.width/2 , x2 = this.world.width/2 + menu.width/2,
+	                y1 = this.world.height/2 - menu.height/2, y2 = this.world.height/2 + menu.height/2;
+	
+	            // Check if the click was inside the menu
+	            if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
+	                // The choicemap is an array that will help us see which
+					// item
+					// was clicked
+	                var choisemap = ['one', 'two', 'three', 'four'];
+	
+	                // Get menu local coordinates for the click
+	                var x = event.x - x1,
+	                    y = event.y - y1;
+	
+	                // Calculate the choice
+	                var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
+	
+	                // Display the choice
+	                choiseLabel.text = 'You chose menu item: ' + choisemap[choise];
+	            }
+	            else{
+	                // Remove the menu and the label
+	                menu.destroy();
+	                choiseLabel.destroy();
+	
+	                // Unpause the game
+	                this.paused = false;
+	            }
+	        }
+	    }
     },
 
     update: function () {
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        // Honestly, just about anything could go here. It's YOUR game after
+		// all. Eat your heart out!
     },
 
     quitGame: function (pointer) {
+        // Here you should destroy anything you no longer need.
+        // Stop music, delete sprites, purge caches, free resources, all that
+		// good stuff.
+    	terminal.destroy();
 
-        //  Here you should destroy anything you no longer need.
-        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-        terminal.destroy();
-
-        //  Then let's go back to the main menu.
+        // Then let's go back to the main menu.
         this.state.start('MainMenu');
 
     }
--- MainMenu.js
+++ MainMenu.js
@@ -1,27 +1,30 @@
 
 BasicGame.MainMenu = function (game) {
-    this.playButton = null;
+    
 };
 
 BasicGame.MainMenu.prototype = {
 
     create: function () {
 
-        //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
-        //	Here all we're doing is playing some music and adding a picture and button
-        //	Naturally I expect you to do something significantly better :)
+        // We've already preloaded our assets, so let's kick right into the Main
+		// Menu itself.
+        // Here all we're doing is playing some music and adding a picture and
+		// button
+        // Naturally I expect you to do something significantly better :)
 
-        this.playButton = this.add.button(200, 300, 'playButton', this.startGame, this);
+        playButton = this.add.button(this.world.width/2, this.world.height/2, 'playButton', this.startGame, this);
+        playButton.anchor.setTo(0.5, 0.5);
     },
 
     update: function () {
 
-        //	Do some nice funky main menu effect here
+        // Do some nice funky main menu effect here
 
     },
 
     startGame: function (pointer) {
-        //	And start the actual game
+        // And start the actual game
         this.state.start('Game');
 
     }
